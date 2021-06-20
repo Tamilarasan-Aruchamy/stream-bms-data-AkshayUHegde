fd892c3f6ef63d3fcbda29961d313873ff2d12ef
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 16:45:18 +0530

    Update test-validation.cpp

--- Receiver/test-validation.cpp
+++ Receiver/test-validation.cpp
@@ -13,21 +13,21 @@ TEST_CASE("Test the Number scanf function calls")
 {
 	ResetTestInterface();
 	
-	char BSM_InputMessage[15][100]={"{\"charge_rate\": 18.69, \"temp_in_c\": 5.26}",\
-                                    "{\"charge_rate\": 9.69, \"temp_in_c\": 6.26}",\
-                                    "{\"charge_rate\": 10.69, \"temp_in_c\": 7.26}",\
-                                    "{\"charge_rate\": 30.69, \"temp_in_c\": 8.26}",\
-                                    "{\"charge_rate\": 12.69, \"temp_in_c\": 9.26}",\
-                                    "{\"charge_rate\": 13.69, \"temp_in_c\": 10.26}",\
-                                    "{\"charge_rate\": 14.69, \"temp_in_c\": 11.26}",\
-                                    "{\"charge_rate\": 15.69, \"temp_in_c\": 12.26}",\
-                                    "{\"charge_rate\": 16.69, \"temp_in_c\": 13.26}",\
-                                    "{\"charge_rate\": 17.69, \"temp_in_c\": 14.26}",\
-                                    "{\"charge_rate\": 18.69, \"temp_in_c\": 15.26}",\
-                                    "{\"charge_rate\": 19.69, \"temp_in_c\": 16.26}",\
-                                    "{\"charge_rate\": 20.69, \"temp_in_c\": 17.26}",\
-                                    "{\"charge_rate\": 21.69, \"temp_in_c\": 18.26}",\
-                                    "{\"charge_rate\": 22.69, \"temp_in_c\": 19.26}"};
+	char BSM_InputMessage[15][100]={"{\"charge_rate\": 11.69, \"temp_in_c\": 5.26}",\
+                                    "{\"charge_rate\": 12.69, \"temp_in_c\": 6.26}",\
+                                    "{\"charge_rate\": 12.69, \"temp_in_c\": 7.26}",\
+                                    "{\"charge_rate\": 14.69, \"temp_in_c\": 8.26}",\
+                                    "{\"charge_rate\": 15.69, \"temp_in_c\": 9.26}",\
+                                    "{\"charge_rate\": 16.69, \"temp_in_c\": 10.26}",\
+                                    "{\"charge_rate\": 17.69, \"temp_in_c\": 11.26}",\
+                                    "{\"charge_rate\": 18.69, \"temp_in_c\": 12.26}",\
+                                    "{\"charge_rate\": 44.69, \"temp_in_c\": 13.26}",\
+                                    "{\"charge_rate\": 23.69, \"temp_in_c\": 14.26}",\
+                                    "{\"charge_rate\": 5.69, \"temp_in_c\": 15.26}",\
+                                    "{\"charge_rate\": 6.69, \"temp_in_c\": 16.26}",\
+                                    "{\"charge_rate\": 7.69, \"temp_in_c\": 17.26}",\
+                                    "{\"charge_rate\": 8.69, \"temp_in_c\": 18.26}",\
+                                    "{\"charge_rate\": 9.69, \"temp_in_c\": 19.26}"};
 	
 	for(int i=0;i<15;i++)
 	{
@@ -100,7 +100,8 @@ TEST_CASE("Test the Min Max and Avg")
 	}
 	
 	
-	
+}
+
 TEST_CASE("Test the wrong Input Message") 
 {
 	ResetTestInterface();
