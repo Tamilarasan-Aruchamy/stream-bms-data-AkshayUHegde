dbd3a280ffc30915c815c429ad9b3b1df273ddc1
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 12:13:48 +0530

    Update test-validation.cpp

--- Receiver/test-validation.cpp
+++ Receiver/test-validation.cpp
@@ -10,6 +10,27 @@ extern int BSM_SignalReceiver();
 
 TEST_CASE("Test the Number scanf function calls") 
 {
-	BSM_SignalReceiver();
+	ResetTestInterface();
+	
+	char BSM_InputMessage[15][100]={"{\"charge_rate\": 18.69, \"temp_in_c\": 5.26}",\
+                                    "{\"charge_rate\": j9.69, \"temp_in_c\": 6.26}",\
+                                    "{\"charge_rate\": 10.69, \"temp_in_c\": 7.26}",\
+                                    "{\"charge_rate\": 199.69, \"temp_in_c\": 8.26}",\
+                                    "{\"charge_rate\": 12.69, \"temp_in_c\": 9.26}",\
+                                    "{\"charge_rate\": 13.69, \"temp_in_c\": 10.26}",\
+                                    "{\"charge_rate\": 14.69, \"temp_in_c\": 11.26}",\
+                                    "{\"charge_rate\": 15.69, \"temp_in_c\": 12.26}",\
+                                    "{\"charge_rate\": 16.69, \"temp_in_c\": 13.26}",\
+                                    "{\"charge_rate\": 17.69, \"temp_in_c\": 14.26}",\
+                                    "{\"charge_rate\": 18.69, \"temp_in_c\": 15.26}",\
+                                    "{\"charge_rate\": 19.69, \"temp_in_c\": 16.26}",\
+                                    "{\"charge_rate\": 20.69, \"temp_in_c\": 17.26}",\
+                                    "{\"charge_rate\": 21.69, \"temp_in_c\": 18.26}",\
+                                    "{\"charge_rate\": 22.69, \"temp_in_c\": 19.26}"};
+	
+	InputMessageBuf=BSM_InputMessage;
+	
+	REQUIRE(BSM_SignalReceiver()==0);
+	REQUIRE(printf_Func_CallCount==15);
 }
 
