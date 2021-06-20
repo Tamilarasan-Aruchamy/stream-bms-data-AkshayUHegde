9879776508dd54a9c54017c3e999a546387577df
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 13:21:46 +0530

    Update test-validation.cpp

--- Receiver/test-validation.cpp
+++ Receiver/test-validation.cpp
@@ -36,8 +36,8 @@ TEST_CASE("Test the Number scanf function calls")
 	REQUIRE(BSM_SignalReceiver()==1);
 	REQUIRE(scanf_Func_CallCount==15);
 	REQUIRE(strcmp(ConsoleInputFormat[1],"%s")==0);
-	REQUIRE(strcmp(ConsoleInputValue[1],BSM_InputMessage[1])==0);
+	//REQUIRE(strcmp(ConsoleInputValue[1],BSM_InputMessage[1])==0);
 	REQUIRE(printf_Func_CallCount==15);
-	//REQUIRE(ConsoleOutputTemperatureMin[1]==10.69);
+	REQUIRE(ConsoleOutputTemperatureMin[1]==10.69);
 }
 
