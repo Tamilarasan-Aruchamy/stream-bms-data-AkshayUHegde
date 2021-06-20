8b89012c9fa7f2dbe00e80eaa01ece3683d0a0c5
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 09:19:53 +0530

    Update BSM_SignalReceiver.c

--- Receiver/BSM_SignalReceiver.c
+++ Receiver/BSM_SignalReceiver.c
@@ -49,7 +49,7 @@ int BSM_SignalReceiver()
 
   int pos = 0;
   float sum[2] = {0};
-  int len=0;
+  int len=5;
 
     for(int i=0; i<5 ; i++)
 	{
@@ -73,10 +73,10 @@ int BSM_SignalReceiver()
                 Calculate_MinMax(InputData[1].SignalValue, &ChargeRate);
 
                
-               if(len<5)
-                    {
-                    len++;
-                    }
+            //   if(len<5)
+              //      {
+             //       len++;
+             //       }
                 Temp.MovingAvg = movingAvg(arrNumbers[0], &sum[0], pos, len, InputData[0].SignalValue);
                // printf("%f\n", sum[0]);
                 ChargeRate.MovingAvg = movingAvg(arrNumbers[1], &sum[1], pos, len, InputData[1].SignalValue);
