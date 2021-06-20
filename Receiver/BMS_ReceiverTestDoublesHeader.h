f283458af7c6c394a077219326bec8ab62750999
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 10:26:25 +0530

    Update BMS_ReceiverTestDoublesHeader.h

--- Receiver/BMS_ReceiverTestDoublesHeader.h
+++ Receiver/BMS_ReceiverTestDoublesHeader.h
@@ -1,15 +1,14 @@
-#include <stdint.h>
-#include <limits.h>
-#define RAND_MAX INT_MAX
-
+extern char InputMessageBuf[15][100];
 extern char* ConsoleOutputFormat[15];
-extern float ConsoleOutputTemperature[15];
-extern float ConsoleOutputSOC[15];
+extern float	ConsoleOutputTemperatureMin[15];
+extern float	ConsoleOutputTemperatureMax[15];
+extern float	ConsoleOutputTemperatureMovingAvg[15];
+extern float	ConsoleOutputChargeRateMin[15];
+extern float	ConsoleOutputChargeRateMax[15];
+extern float	ConsoleOutputChargeRateMovingAvg[15];
 extern int printf_Func_CallCount;
 
 
 extern int ResetTestInterface();
-extern int rand();
-extern int printf(char* Format, float Temperature, float SOC);
-
-
+extern int scanf(const char *format, char * InputBuffer);
+extern int printf(const char *format, float Temp_MinValue, float Temp_MinValue, float Temp_MovingAvg, float ChargeRate_MinValue, float ChargeRate_MinValue, float ChargeRate_MovingAvg);
