f7ab08e76d86e786725fb1487e156feafb6f680b
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 17:12:03 +0530

    Update test-validation.cpp

--- Receiver/test-validation.cpp
+++ Receiver/test-validation.cpp
@@ -9,36 +9,7 @@
 extern int BSM_SignalReceiver();
 
 
-TEST_CASE("Test the Number scanf function calls") 
-{
-	ResetTestInterface();
-	
-	char BSM_InputMessage[15][100]={"{\"charge_rate\": 11.69, \"temp_in_c\": 5.26}",\
-                                    "{\"charge_rate\": 12.69, \"temp_in_c\": 6.26}",\
-                                    "{\"charge_rate\": 12.69, \"temp_in_c\": 7.26}",\
-                                    "{\"charge_rate\": 14.69, \"temp_in_c\": 8.26}",\
-                                    "{\"charge_rate\": 15.69, \"temp_in_c\": 9.26}",\
-                                    "{\"charge_rate\": 16.69, \"temp_in_c\": 10.26}",\
-                                    "{\"charge_rate\": 17.69, \"temp_in_c\": 11.26}",\
-                                    "{\"charge_rate\": 18.69, \"temp_in_c\": 12.26}",\
-                                    "{\"charge_rate\": 44.69, \"temp_in_c\": 13.26}",\
-                                    "{\"charge_rate\": 23.69, \"temp_in_c\": 14.26}",\
-                                    "{\"charge_rate\": 5.69, \"temp_in_c\": 15.26}",\
-                                    "{\"charge_rate\": 6.69, \"temp_in_c\": 16.26}",\
-                                    "{\"charge_rate\": 7.69, \"temp_in_c\": 17.26}",\
-                                    "{\"charge_rate\": 8.69, \"temp_in_c\": 18.26}",\
-                                    "{\"charge_rate\": 9.69, \"temp_in_c\": 19.26}"};
-	
-	for(int i=0;i<15;i++)
-	{
-	strcpy(InputMessageBuf[i],BSM_InputMessage[i]);
-	}
-	
-	REQUIRE(BSM_SignalReceiver()==1);
-	REQUIRE(scanf_Func_CallCount==15);
-}
-
-TEST_CASE("Test the Min Max and Avg") 
+TEST_CASE("Test the Func Call,Min, Max and Avg") 
 {
 	ResetTestInterface();
 	
@@ -81,6 +52,8 @@ TEST_CASE("Test the Min Max and Avg")
 	}
 	
 	REQUIRE(BSM_SignalReceiver()==1);
+	
+	REQUIRE(scanf_Func_CallCount==8);
 	REQUIRE(printf_Func_CallCount==15);
 	
 	for(int i=0;i<15;i++)
