eb7a11885bac51f9c908afbbda183328a1b8b3b0
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 13:29:38 +0530

    Update BMS_ReceiverTestDoublesHeader.h

--- Receiver/BMS_ReceiverTestDoublesHeader.h
+++ Receiver/BMS_ReceiverTestDoublesHeader.h
@@ -1,7 +1,7 @@
 extern char InputMessageBuf[15][100];
-extern char* ConsoleInputFormat[15];
-extern char* ConsoleInputValue[15];
-extern char* ConsoleOutputFormat[15];
+extern char* ConsoleInputFormat[15][5];
+extern char* ConsoleInputValue[15][100];
+extern char* ConsoleOutputFormat[15][200];
 extern float	ConsoleOutputTemperatureMin[15];
 extern float	ConsoleOutputTemperatureMax[15];
 extern float	ConsoleOutputTemperatureMovingAvg[15];
