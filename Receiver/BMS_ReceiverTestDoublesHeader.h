5d5d50f61a35e303558d0c2a6349acd7f413d144
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-06-20 18:51:35 +0530

    Update BMS_ReceiverTestDoublesHeader.h

--- Receiver/BMS_ReceiverTestDoublesHeader.h
+++ Receiver/BMS_ReceiverTestDoublesHeader.h
@@ -1,16 +1,16 @@
 extern char InputMessageBuf[15][100];
 extern char ConsoleInputFormat[15][5];
 extern char ConsoleOutputFormat[15][200];
-extern float	ConsoleOutputTemperatureMin[15];
-extern float	ConsoleOutputTemperatureMax[15];
-extern float	ConsoleOutputTemperatureMovingAvg[15];
 extern float	ConsoleOutputChargeRateMin[15];
 extern float	ConsoleOutputChargeRateMax[15];
 extern float	ConsoleOutputChargeRateMovingAvg[15];
+extern float	ConsoleOutputTemperatureMin[15];
+extern float	ConsoleOutputTemperatureMax[15];
+extern float	ConsoleOutputTemperatureMovingAvg[15];
 extern int printf_Func_CallCount;
 extern int scanf_Func_CallCount;
 
 
 extern int ResetTestInterface();
 extern int scanf(char *format, char * InputBuffer);
-extern int printf(char *format, float Temp_MinValue, float Temp_MaxValue, float Temp_MovingAvg, float ChargeRate_MinValue, float ChargeRate_MaxValue, float ChargeRate_MovingAvg);
+extern int printf(char *format, float ChargeRate_MinValue, float ChargeRate_MaxValue, float ChargeRate_MovingAvg, float Temp_MinValue, float Temp_MaxValue, float Temp_MovingAvg);
